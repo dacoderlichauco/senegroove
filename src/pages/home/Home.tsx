a0b67123e94c35315332f38038620ae27b8b6b87
dacoderlichauco <115046653+dacoderlichauco@users.noreply.github.com>
2024-06-12 10:49:49 -0400

    edited home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,8 +6,13 @@ import Navbar from "../Navbar";
 function Home() {
   return (
     <>
-      <Navbar></Navbar>
-    </ >
+      <Navbar />
+      <div className="flex items-center justify-center h-screen lightblue">
+        <h1 className="text-white text-8xl animate-fade-in">
+          Welcome to Senegroove!
+        </h1>
+      </div>
+    </>
   );
 }
 
